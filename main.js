b5f91f1dc629022edb844cc7aa673b460164ab8d
bizarf <snesmaster40@googlemail.com>
2022-10-04 00:23:05 +0100

    Finished the coding. Sorted out the bugs

--- main.js
+++ main.js
@@ -10,6 +10,7 @@ var __webpack_exports__ = {};
     const errorMessage = document.querySelectorAll(".error")
     const errorIcon = document.querySelectorAll(".fa-circle-exclamation")
     const checkMark = document.querySelectorAll(".fa-circle-check")
+    const sucessfulSubmit = document.querySelector(".sucessfulSubmit")
 
     // email field
     email.addEventListener("input", () => {
@@ -20,8 +21,7 @@ var __webpack_exports__ = {};
         } else {
             errorMessage[0].style.display = "none";
             errorIcon[0].style.display = "none"
-            email.style.borderColor = "none"
-            email.style.outlineColor = "none"
+            email.style.borderColor = ""
         }
 
         if (email.validity.valid) {
@@ -31,19 +31,22 @@ var __webpack_exports__ = {};
             checkMark[0].style.display = "none"
         }
 
-        if (email.value === "") {
+        if (email.validity.valueMissing) {
             checkMark[0].style.display = "none"
         }
     })
 
-    // country.addEventListener("input", () => {
-    //     if (country.validity.valueMissing) {
-    //         country.setCustomValidity("Please type in a country");
-    //         country.reportValidity()
-    //     } else {
-    //         country.setCustomValidity("")
-    //     }
-    // })
+    country.addEventListener("input", () => {
+        if (country.validity.valid) {
+            errorMessage[1].style.display = "none"
+            errorIcon[1].style.display = "none"
+            checkMark[1].style.display = "block";
+        }
+
+        if (country.validity.valueMissing) {
+            checkMark[1].style.display = "none"
+        }
+    })
 
     areaCode.addEventListener("input", () => {
         if (areaCode.validity.patternMismatch) {
@@ -51,25 +54,54 @@ var __webpack_exports__ = {};
             errorMessage[2].textContent = "Please type in a valid postcode or zip code";
             errorIcon[2].style.display = "block"
             areaCode.style.borderColor = "#dc2626"
+            checkMark[2].style.display = "none";
         } else {
             errorMessage[2].style.display = "none";
             errorIcon[2].style.display = "none"
-            areaCode.style.borderColor = "none"
-            // areaCode.style.outlineColor = "none"
+            checkMark[2].style.display = "block";
+            areaCode.style.borderColor = ""
+        }
+
+        if (areaCode.validity.valueMissing) {
+            checkMark[2].style.display = "none";
         }
     })
 
     password.addEventListener("input", () => {
-        if (!password.value < 8) {
+        if (password.validity.tooShort) {
             errorMessage[3].style.display = "block";
             errorMessage[3].textContent = "Please type in a password that is more than 8 characters";
             errorIcon[3].style.display = "block"
             password.style.borderColor = "#dc2626"
+            checkMark[3].style.display = "none"
         } else {
-            errorMessage[2].style.display = "none";
-            errorIcon[2].style.display = "none"
-            password.style.borderColor = "none"
-            password.style.outlineColor = "none"
+            errorMessage[3].style.display = "none";
+            errorIcon[3].style.display = "none"
+            password.style.borderColor = ""
+            checkMark[3].style.display = "block"
+        }
+
+        if (password.validity.valueMissing) {
+            checkMark[3].style.display = "none"
+        }
+    })
+
+    passwordConfirmation.addEventListener("input", () => {
+        if (passwordConfirmation.value !== password.value) {
+            errorMessage[4].style.display = "block";
+            errorMessage[4].textContent = "Your password does not match";
+            errorIcon[4].style.display = "block"
+            passwordConfirmation.style.borderColor = "#dc2626"
+            checkMark[4].style.display = "none"
+        } else {
+            errorMessage[4].style.display = "none";
+            errorIcon[4].style.display = "none"
+            passwordConfirmation.style.borderColor = ""
+            checkMark[4].style.display = "block"
+        }
+
+        if (passwordConfirmation.validity.valueMissing) {
+            checkMark[4].style.display = "none"
         }
     })
 
@@ -81,6 +113,12 @@ var __webpack_exports__ = {};
         blankValueError(areaCode, 2, "Please enter a postcode or zip code")
         blankValueError(password, 3, "Please enter a password")
         blankValueError(passwordConfirmation, 4, "Please type in the above password to ")
+
+        if (email.validity.valid && country.validity.valid && areaCode.validity.valid && password.validity.valid && passwordConfirmation.validity.valid) {
+            sucessfulSubmit.style.display = "block";
+        } else {
+            sucessfulSubmit.style.display = "none";
+        }
     })
 
     function blankValueError(domElement, domElementValue, message) {
